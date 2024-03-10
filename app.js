1be9bf58bcd5c6eee34a45d15458db8b47ab131e
Diogo Ferreira <diogomf@hey.com>
2024-03-10 22:10:18 +0000

    add multiple tokens: sol, eth

--- app.js
+++ app.js
@@ -8,8 +8,17 @@ registerFont("./public/fonts/uncut-sans.ttf", { family: "Uncut Sans" });
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const COINGECKO_API =
-  "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd";
+const COINGECKO_BASE_URL = "https://api.coingecko.com/api/v3/simple/";
+
+const coingeckoApi = `${COINGECKO_BASE_URL}/price?ids=ethereum,bitcoin,solana&vs_currencies=usd`;
+
+const tokenText = price => {
+  const formattedPrice = price.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+  return formattedPrice;
+};
 
 // Serve static files from the "public" directory
 app.use(express.static("public"));
@@ -25,16 +34,19 @@ app.get("/test", (req, res) => {
 });
 
 app.get("/image", async (req, res) => {
-  let bitcoinText = "Bitcoin - $69,392.34";
-  try {
-    // Fetch current price of Bitcoin
-    const response = await axios.get(COINGECKO_API);
+  let bitcoinText = "$69,420";
+  let ethText = "$4,200";
+  let solText = "$4,20";
 
-    console.log(response);
+  try {
+    // Fetch current price of tokens
+    const response = await axios.get(coingeckoApi);
+
     const { data } = response;
-    if (data && data.bitcoin) {
-      console.log("bitcoin price", data.bitcoin.usd);
-      bitcoinText = `Bitcoin - $${data.bitcoin.usd}`;
+    if (data && data.bitcoin && data.ethereum) {
+      bitcoinText = tokenText(data.bitcoin.usd);
+      ethText = tokenText(data.ethereum.usd);
+      solText = tokenText(data.solana.usd);
     }
   } catch (error) {
     console.error("Error creating image:", error);
@@ -53,27 +65,53 @@ app.get("/image", async (req, res) => {
   context.fillRect(0, 0, width, height);
 
   const imagePathBitcoin = path.join(__dirname, "public", "images/bitcoin.png");
-  // const imagePathETH = path.join(__dirname, "public", "images/tokens.png");
+  const imagePathETH = path.join(__dirname, "public", "images/eth.png");
+  const imagePathSOL = path.join(__dirname, "public", "images/sol.png");
 
   // Load an image from file or URL
   const imageBitcoin = await loadImage(imagePathBitcoin);
-  // const imageETH = await loadImage(imagePathETH);
+  const imageETH = await loadImage(imagePathETH);
+  const imageSOL = await loadImage(imagePathSOL);
 
   // Draw the image onto the canvas
-  // context.drawImage(imageBitcoin, 0, 0, width, height);
-  const tokenImageSize = 99;
-  context.drawImage(imageBitcoin, 50, 80, tokenImageSize, tokenImageSize);
+  const tokenImageSize = 110;
+  const imagePaddingLeft = 50;
 
-  // Set token text properties
-  context.font = "48px Uncut Sans";
+  context.drawImage(
+    imageBitcoin,
+    imagePaddingLeft,
+    30,
+    tokenImageSize,
+    tokenImageSize
+  );
+  context.drawImage(
+    imageETH,
+    imagePaddingLeft,
+    170,
+    tokenImageSize,
+    tokenImageSize
+  );
+  context.drawImage(
+    imageSOL,
+    imagePaddingLeft,
+    330,
+    tokenImageSize,
+    tokenImageSize
+  );
+
+  // Set font properties
+  context.font = "84px Uncut Sans";
   context.fillStyle = "#000000"; // black tex
   // Add text to the canvas
-  context.fillText(bitcoinText, 180, 150);
+  const textPaddingLeft = 248;
+  context.fillText(bitcoinText, textPaddingLeft, 120);
+  context.fillText(ethText, textPaddingLeft, 260);
+  context.fillText(solText, textPaddingLeft, 420);
 
   // green percentage
-  context.font = "48px Uncut Sans";
-  context.fillStyle = "#00A83E"; // black tex
-  context.fillText("+3%", 550, 150);
+  // context.font = font;
+  // context.fillStyle = "#00A83E"; // black tex
+  // context.fillText("+3%", 600, 150);
 
   // Convert the canvas to a Buffer and send it as the response
   const buffer = canvas.toBuffer("image/png");
